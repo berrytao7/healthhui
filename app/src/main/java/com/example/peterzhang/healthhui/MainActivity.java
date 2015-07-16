2966abc4b4e82ddf432586422b8ef49c8e54d3dd
peterzhang <peterzhang@eaglenet.cn>
2015-07-16 10:55:34 +0800

    增加抽屉

--- app/src/main/java/com/example/peterzhang/healthhui/MainActivity.java
+++ app/src/main/java/com/example/peterzhang/healthhui/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.peterzhang.healthhui;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,10 +24,23 @@ public class MainActivity extends ActionBarActivity {
 
     private static final String TAG = "MainActivity";
     private GridView mGridView;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            toolbar.setNavigationIcon(R.drawable.ic_ab_drawer);
+        }
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerLayout.setDrawerListener(drawerToggle);
+
         mGridView = (GridView)this.findViewById(R.id.gridView);
         ArrayList<HashMap<String,Object>> itemList = new ArrayList<HashMap<String,Object>>();
         HashMap<String,Object> mapHealth = new HashMap<String,Object>();
@@ -42,7 +62,29 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         });
-        getSupportActionBar().setBackgroundDrawable(this.getResources().getDrawable(R.color.actionbar_bg));
+
+        mDrawerListView = (ListView)this.findViewById(R.id.navdrawer);
+        String[] values = new String[]{
+                this.getResources().getString(R.string.health_search)
+        };
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        mDrawerListView.setAdapter(listAdapter);
+        mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent();
+                        intent.setClass(MainActivity.this,HealthMsgActivity.class);
+                        MainActivity.this.startActivity(intent);
+                        mDrawerLayout.closeDrawer(Gravity.LEFT);
+                        break;
+                }
+
+            }
+        });
 
 //        BuildConfig
     }
@@ -65,8 +107,22 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }else if(id == android.R.id.home){
+            mDrawerLayout.openDrawer(Gravity.START);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
